8c04fc4740326684eb9a2066fed3e55f44eb7422
omerrevach <hairviction@gmail.com>
2025-03-31 01:49:00 +0300

    fixed ec2 private ip output

--- tf/modules/ec2/outputs.tf
+++ tf/modules/ec2/outputs.tf
@@ -2,8 +2,8 @@ output "instance_ids" {
   value = { for k, inst in aws_instance.nginx : k => inst.id }
 }
 
-output "private_ip" {
-  value       = aws_instance.nginx.private_ip
+output "private_ips" {
+  value = { for k, inst in aws_instance.nginx : k => inst.private_ip }
 }
 
 output "security_group_id" {
