e25bb9b76a94feac185c7727a4de40fcc09b2977
omerrevach <hairviction@gmail.com>
2025-03-31 15:28:51 +0300

    removed ec2 module no longer needed

--- tf/modules/ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_ids" {
-  value = aws_instance.nginx[*].id
-}
-output "private_ips" {
-  value = aws_instance.nginx[*].private_ip
-}
-output "security_group_id" {
-  value       = aws_security_group.ec2_sg.id
-}
