269a823e828e87eedeb5c5a6fce681510717d5ef
omerrevach <hairviction@gmail.com>
2025-03-31 13:55:59 +0300

    fixed error with ec2

--- tf/main.tf
+++ tf/main.tf
@@ -1,18 +1,32 @@
 module "vpc" {
-  source  = "./modules/vpc"
+  source = "./modules/vpc"
 
-  vpc_name = var.vpc_name
-  cidr = var.cidr
+  vpc_name            = var.vpc_name
+  cidr                = var.cidr
+  azs                 = var.azs
+  private_subnets     = var.private_subnets
+  public_subnets      = var.public_subnets
+  enable_nat_gateway  = var.enable_nat_gateway
+  single_nat_gateway  = var.single_nat_gateway
+  one_nat_gateway_per_az = var.one_nat_gateway_per_az
+  environment         = var.environment
+}
 
-  azs             = var.azs
-  private_subnets = var.private_subnets
-  public_subnets  = var.public_subnets
+module "ecr" {
+  source    = "./modules/ecr"
+  repo_name = "nginx-assignment"
+}
 
-  enable_nat_gateway         = var.enable_nat_gateway
-  single_nat_gateway         = var.single_nat_gateway
-  one_nat_gateway_per_az     = var.one_nat_gateway_per_az
+module "iam" {
+  source       = "./modules/iam"
+  ecr_repo_arn = module.ecr.repository_arn
+}
 
-  environment            = var.environment
+module "alb" {
+  source              = "./modules/alb"
+  vpc_id              = module.vpc.vpc_id
+  public_subnet_ids   = module.vpc.public_subnet_ids
+  acm_certificate_arn = data.aws_acm_certificate.this.arn
 }
 
 module "asg" {
@@ -27,53 +41,19 @@ module "asg" {
   alb_target_group_arn  = module.alb.target_group_arn
 }
 
-# module "ec2" {
-#   source                = "./modules/ec2"
-#   vpc_id                = module.vpc.vpc_id
-#   subnet_ids            = module.vpc.private_subnet_ids
-#   linux_ami             = var.linux_ami
-#   instance_type         = var.instance_type
-#   alb_sg_id             = module.alb.alb_sg_id
-#   instance_profile_name = module.iam.instance_profile_name
-#   ecr_repo_url          = module.ecr.repository_url
-
-#   depends_on = [ module.ecr ]
-# }
-
 module "vpc_endpoints" {
   source                = "./modules/vpc_endpoints"
   vpc_id                = module.vpc.vpc_id
   subnet_ids            = module.vpc.private_subnet_ids
-  ec2_security_group_id = module.ec2.security_group_id
+  ec2_security_group_id = module.asg.security_group_id
   region                = var.region
   name_prefix           = "ssm"
 }
 
-
-module "iam" {
-  source       = "./modules/iam"
-  ecr_repo_arn = module.ecr.repository_arn
-}
-
-module "alb" {
-  source              = "./modules/alb"
-  vpc_id              = module.vpc.vpc_id
-  public_subnet_ids   = module.vpc.public_subnet_ids
-  acm_certificate_arn = data.aws_acm_certificate.this.arn
-}
-
-
-
 module "route53" {
-  source          = "./modules/route53"
-  hosted_zone_id  = var.hosted_zone_id
-  domain_name     = var.domain_name
-  alb_dns_name    = module.alb.alb_dns_name
-  alb_zone_id     = module.alb.alb_zone_id
+  source         = "./modules/route53"
+  hosted_zone_id = var.hosted_zone_id
+  domain_name    = var.domain_name
+  alb_dns_name   = module.alb.alb_dns_name
+  alb_zone_id    = module.alb.alb_zone_id
 }
-
-module "ecr" {
-  source    = "./modules/ecr"
-  repo_name = "nginx-assignment"
-}
-
--- tf/modules/asg/outputs.tf
+++ tf/modules/asg/outputs.tf
@@ -1,3 +1,7 @@
 output "asg_name" {
   value = aws_autoscaling_group.asg.name
 }
+
+output "security_group_id" {
+  value = aws_security_group.asg_sg.id
+}
